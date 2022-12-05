ba01767d0b8ff335cab65131be96b2d54f64c8c3
john Caropino <jccaropino7786@live.com>
2022-12-05 15:28:20 -0800

    updated fetch and db.json

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const ulList = document.querySelector("#links > ul")
+
+function displayDrinks(drinkObj){
+    const drinkImg = document.createElement("img")
+    drinkImg.src = drinkObj.imageUrl
+    drinkImg.alt = drinkObj.name
+    ulList.appendChild(drinkImg)
+}
+
+
+//fetch function
+const fetchDrinks = () => {
+    fetch("http://localhost:3000/drinks")
+    // .then(res => console.log(res))}
+    .then(response =>response.json())
+    .then(drinks => drinks.forEach(displayDrinks))
+    .catch(error => alert(error))
+    }
+
+fetchDrinks()
