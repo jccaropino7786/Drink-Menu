d6c46d05a913735030c037f85b786231c0950b4c
john Caropino <jccaropino7786@live.com>
2022-12-06 09:29:19 -0800

    wrote some simple js update image size in css a little container restrictions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ulList = document.querySelector("#links > ul")
-
+const containerOne = document.querySelector("#container1")
 
 function displayDrinks(drinkObj){
     const drinkImg = document.createElement("img")
@@ -8,14 +8,25 @@ function displayDrinks(drinkObj){
     ulList.appendChild(drinkImg)
 }
 
+function displayFirstDrink(drinkObj){
+    const drinkImg = document.createElement("img")
+    drinkImg.src = drinkObj.imageUrl
+    drinkImg.alt = drinkObj.name
+    containerOne.appendChild(drinkImg)
+
+}
 
 //fetch function
 const fetchDrinks = () => {
     fetch("http://localhost:3000/drinks")
     // .then(res => console.log(res))}
     .then(response =>response.json())
-    .then(drinks => drinks.forEach(displayDrinks))
-    .catch(error => alert(error))
+    .then(drinks => {
+        displayFirstDrink(drinks[0])
+        drinks.forEach(displayDrinks)
+    })
+    // .then(drinks => drinks.forEach(displayDrinks))
+    // .catch(error => alert(error))
     }
 
 fetchDrinks()
