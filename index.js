5304d79fd850a2c91e129e51c4578fa8faec93a2
john Caropino <jccaropino7786@live.com>
2022-12-06 14:10:26 -0800

    small style change

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const ulList = document.querySelector("#links > ul")
 const containerOne = document.querySelector("#container1")
+const containerTwo = document.querySelector("#container2")
+const drinkName = document.querySelector("#container2 > h1")
+const drinkDirections = document.querySelector("#container2 > h2")
+const drinkIngredients = document.querySelector("#ingredientsContainer")
+
+function handleClick(drink){
+    containerOne.innerHTML = ("")
+    const drinkImg = document.createElement("img")
+    drinkImg.src = drink.imageUrl
+    drinkImg.alt = drink.name
+    containerOne.appendChild(drinkImg)
+
+    
+    drinkName.innerText = drink.name
+    drinkDirections.innerText = drink.instruction
+    drinkIngredients.innerText = ("")
+    
+    
+    drink.ingredient.forEach(displayIngredient(drink.ingredient))
+        
+    // drink.forEach(displayIngerdients(drink.ingredient)) 
+}
+
+function displayIngredient(ingredient){
+    const inLi = document.createElement("li")
+        inLi.innerText = ingredient.ingredient
+        drinkIngredients.appendChild(inLi)
+    
+}
+
 
 function displayDrinks(drinkObj){
     const drinkImg = document.createElement("img")
     drinkImg.src = drinkObj.imageUrl
     drinkImg.alt = drinkObj.name
     ulList.appendChild(drinkImg)
+    drinkImg.addEventListener('click', (e) => handleClick(drinkObj, e))
 }
 
 function displayFirstDrink(drinkObj){
@@ -13,6 +44,8 @@ function displayFirstDrink(drinkObj){
     drinkImg.src = drinkObj.imageUrl
     drinkImg.alt = drinkObj.name
     containerOne.appendChild(drinkImg)
+    drinkName.innerText = drinkObj.name
+    drinkDirections.innerText = drinkObj.instruction
 
 }
 
