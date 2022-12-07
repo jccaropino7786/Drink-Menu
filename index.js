e5ae9456373d6931b9fcf8d4b8a10b4ff0ab3f27
Griffin Mcarthur <Griffinmcarthur1@gmail.com>
2022-12-07 16:10:37 -0700

    submit button fixes

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const ulList = document.querySelector("#links > ul")
+const drinkImage = document.getElementById("drink-image")
+const drinkName = document.getElementById("drink-name")
+const drinkinstructions = document.getElementById("drink-instructions")
+const drinkingredients = document.getElementById("drink-ingredients")
+const newDrinksForm = document.getElementById("container3")
+
+const handleClick= (drink) => {
+    
+}
 
 
 function displayDrinks(drinkObj){
     const drinkImg = document.createElement("img")
     drinkImg.src = drinkObj.imageUrl
-    drinkImg.alt = drinkObj.name
+    drinkImg.alt = drinkObj.name 
+    drinkImg.addEventListener("click", () => handleClick(drinkObj))
     ulList.appendChild(drinkImg)
+   
 }
+
+
 
 
 //fetch function
@@ -19,3 +32,5 @@ const fetchDrinks = () => {
     }
 
 fetchDrinks()
+
+
